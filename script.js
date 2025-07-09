55ad994aa90fdb082aa971417627f6b6b0043c18
Brenda Hidalgo <brendascarante99@gmail.com>
2025-07-08 21:05:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const segredo = Math.floor(Math.random() * 100) + 1;
 function verificarPalpite() {
-const palpite = parseInt(document.getElementById('palpite').value);
-let feedback = '';
-if (palpite === segredo) {
-    feedback = 'Parabéns! Você acertou!';
-} else if (palpite > segredo) {
-    feedback = 'Tente um número menor.';
-} else {
-    feedback = 'Tente um número maior.';
-}
-document.getElementById('feedback').style.display = 'inline-block';
-document.getElementById('feedback').textContent = feedback;
+    const palpite = parseInt(document.getElementById('palpite').value);
+    let feedback = '';
+    if (palpite === segredo) {
+        feedback = 'Parabéns! Você acertou!';
+    } else if (palpite > segredo) {
+        feedback = 'Tente um número menor.';
+    } else {
+        feedback = 'Tente um número maior.';
+    }
+    document.getElementById('feedback').style.display = 'inline-block';
+    document.getElementById('feedback').textContent = feedback;
 }
